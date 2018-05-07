efe97d84cca71e61be986eafe16cb1ea51634eae
Pascal Hertleif <killercup@gmail.com>
2018-05-07 15:22:26 +0200

    Add tests for migration lints
    We should late on pass `-W rust_2018_migration` automatically when there
    are lints present in this lint group.

---
+++ tests/migration/absolute_path_starting_with_module.rs
@@ -0,0 +1,14 @@
+#![allow(unused)]
+#![feature(crate_in_paths)]
+#![warn(absolute_path_starting_with_module)]
+// #![warn(rust_2018_migration)]
+
+mod foo {
+    use ::bar::Bar;
+}
+
+pub mod bar {
+    pub struct Bar;
+}
+
+fn main() {}
